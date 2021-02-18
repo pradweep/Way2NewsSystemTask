3f5202cb27efc56c6e480a1b9819999695cf4036
pradeepiOS <pradeep.iostech@gmail.com>
2021-02-18 13:15:29 +0530

    Initial Commit

---
+++ Way2SmsSystemTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Way2SmsSystemTask
+//
+//  Created by Pradeep's Macbook on 18/02/21.
+//  Copyright © 2021 Motiv Ate Fitness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
